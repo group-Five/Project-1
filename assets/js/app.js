20f0102ff789337ab34ad46c1e15c3e09c867515
Tim Kellogg <timmkellogg@gmail.com>
2017-09-06 18:25:14 -0400

    new project files

---
+++ assets/js/app.js
@@ -0,0 +1,10 @@
+//Switches from instructions screen to game screen
+var toggle = function(){
+	$('.main').removeClass('hidden');
+	$('.instructions').addClass('hidden');
+}
+
+//
+var playGame = function(){
+
+}
