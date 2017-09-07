d54a7e4a1044273e090a224781bd5e5c2797f393
timmkellogg <timmkellogg@gmail.com>
2017-09-07 16:23:59 -0400

    added js to push poster to page on game start

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,28 +1,31 @@
+var masterMovieObject;
+var movieArray = [];
+var randomPageNumber = Math.floor(Math.random() * 300) + 1  
+var queryURL = "https://api.themoviedb.org/3/movie/popular?api_key=ee2e00cb4eb46b7262f08bc8d337cc19&language=en-US&page=" + randomPageNumber;
+
 //Switches from instructions screen to game screen
 var toggle = function(){
 	$('.main').removeClass('hidden');
 	$('.instructions').addClass('hidden');
+	playGame();
 }
 
+//Switches from input screen to results screen
 var submitInput = function(){
 	$('.input-screen').addClass('hidden');
 	$('.results-screen').removeClass('hidden');
+	//show results
 }
 
+//Switches from results screen to input screen (new question)
 var nextQuestion = function(){
 	$('.results-screen').addClass('hidden');
 	$('.input-screen').removeClass('hidden');
 }
 
-//
+//Pulls data into movieArray and displays the first question
 var playGame = function(){
-
-}
-var masterMovieObject;
-var movieArray = [];
-var randomPageNumber = Math.floor(Math.random() * 300) + 1  
-var queryURL = "https://api.themoviedb.org/3/movie/popular?api_key=ee2e00cb4eb46b7262f08bc8d337cc19&language=en-US&page=" + randomPageNumber;
-$.ajax({
+	$.ajax({
       url: queryURL,
       method: "GET"
     }).done(function(response) {
@@ -39,9 +42,9 @@ $.ajax({
 	    			},
 	    		}
 	    	movieArray.push(masterMovieObject.movie);
+	    	
     		}
 		}
-    	console.log(movieArray[0].title);
+    	$('.image').html('<img src="https://image.tmdb.org/t/p/w500' + movieArray[0].poster + '"/>');
 	});
-
-console.log(movieArray);
+}
