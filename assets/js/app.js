32abff52fd45ee3babcb87978f025b4875624212
timmkellogg <timmkellogg@gmail.com>
2017-09-08 22:45:09 -0400

    completed game screen flow, addend score tracking

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,25 +1,53 @@
 var masterMovieObject;
 var movieArray = [];
-var randomPageNumber = Math.floor(Math.random() * 300) + 1  
+var randomPageNumber = Math.floor(Math.random() * 300) + 1;
 var queryURL = "https://api.themoviedb.org/3/movie/popular?api_key=ee2e00cb4eb46b7262f08bc8d337cc19&language=en-US&page=" + randomPageNumber;
 var questionCounter = 0;
+var score = 0;
 
-//Switches from instructions screen to game screen
-var toggle = function(){
-	$('.main').removeClass('hidden');
-	$('.instructions').addClass('hidden');
-	playGame();
+//Returns difference
+var difference = function(input, rating){
+	return Math.abs(input - rating);
 }
 
 //Switches from input screen to results screen
 var submitInput = function(){
+	$('.movie-score').text(movieArray[questionCounter].rating);
+
+	if(questionCounter === 9){
+		$('.next').addClass('hidden');
+		$('.end').removeClass('hidden');
+	}
+
 	$('.input-screen').addClass('hidden');
 	$('.results-screen').removeClass('hidden');
-	//show results
+
+	var input = $('#slider').val();
+	var rating = movieArray[questionCounter].rating;
+	var diff = difference(input, rating);
+
+	if(diff === 0){
+		score =+ 100;
+	}
+
+	else if(diff <= 1){
+		score =+ 50;
+	}
+
+	else if(diff <= 2){
+		score =+ 25;
+	}
+
+	else{
+		score =+ 10;
+	}
 }
 
 //Switches from results screen to input screen (new question)
 var nextQuestion = function(){
+	questionCounter++;
+	$('.image').html('<img src="https://image.tmdb.org/t/p/w500' + movieArray[questionCounter].poster + '"/>');
+
 	$('.results-screen').addClass('hidden');
 	$('.input-screen').removeClass('hidden');
 	//Display next question
@@ -27,6 +55,13 @@ var nextQuestion = function(){
 
 //Pulls data into movieArray and displays the first question
 var playGame = function(){
+	questionCounter = 0;
+	score = 0;
+	movieArray = [];
+
+	$('.main').removeClass('hidden');
+	$('.instructions').addClass('hidden');
+
 	$.ajax({
       url: queryURL,
       method: "GET"
@@ -48,7 +83,14 @@ var playGame = function(){
     		}
 		}
     	//shuffle array order
-    	questionCounter++;
-    	$('.image').html('<img src="https://image.tmdb.org/t/p/w500' + movieArray[0].poster + '"/>');
+    	$('.image').html('<img src="https://image.tmdb.org/t/p/w500' + movieArray[questionCounter].poster + '"/>');
 	});
+}
+
+//Runs end of game procedures
+var endGame = function(){
+	$('.score').text('Your score: ' + score);
+
+	$('.main').addClass('hidden');
+	$('.end-screen').removeClass('hidden');
 }
