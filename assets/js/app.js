9a3d47a39e1ae89306f393ebf352fe35266d41e0
Michael Stephenson <mjstephenson@me.com>
2017-09-09 15:20:40 -0400

    Added full Firebase interactivity for Hiscores.

--- assets/js/app.js
+++ assets/js/app.js
@@ -9,13 +9,15 @@ var config = {
 	};
 firebase.initializeApp(config);
 var database = firebase.database();
-
 var masterMovieObject;
 var movieArray = [];
 var randomPageNumber = Math.floor(Math.random() * 300) + 1;
 var queryURL = "https://api.themoviedb.org/3/movie/popular?api_key=ee2e00cb4eb46b7262f08bc8d337cc19&language=en-US&page=" + randomPageNumber;
 var questionCounter = 0;
 var score = 0;
+var lowestScore;
+var userInitials;
+var inverseScore;
 
 //Returns difference
 var difference = function(input, rating){
@@ -37,22 +39,27 @@ var submitInput = function(){
 	var input = $('#slider').val();
 	var rating = movieArray[questionCounter].rating;
 	var diff = difference(input, rating);
-
+	console.log("Before you answer, your score is " + score);
 	if(diff === 0){
-		score =+ 100;
+		score = score + 100;
+		console.log("You get 100 points (best)");
 	}
 
 	else if(diff <= 1){
-		score =+ 50;
+		score = score + 50;
+		console.log("You get 50 points (second best)");
 	}
 
 	else if(diff <= 2){
-		score =+ 25;
+		score = score + 25;
+		console.log("You get 25 points (second worst)");
 	}
 
 	else{
-		score =+ 10;
+		score = score + 10;
+		console.log("You get 10 points (worst)");
 	}
+	console.log("Your score is now " + score);
 }
 
 //Switches from results screen to input screen (new question)
@@ -97,12 +104,79 @@ var playGame = function(){
     	//shuffle array order
     	$('.image').html('<img src="https://image.tmdb.org/t/p/w500' + movieArray[questionCounter].poster + '"/>');
 	});
+    //Making lowestScore equal to zero
+    lowestScore = 0;
+    console.log("Lowest score set to zero by default.");
+	//Grab the lowest score out of Firebase and overwriting lowestScore.  If nothing is pull, lowestScore stays at zero
+	database.ref().orderByChild("score").limitToFirst(1).on("child_added", function(snapshot){
+			lowestScore = snapshot.val().score;
+			console.log("The updated lowest score from Firebase is now " + lowestScore);
+	});
 }
 
 //Runs end of game procedures
 var endGame = function(){
 	$('.score').text('Your score: ' + score);
 
-	$('.main').addClass('hidden');
-	$('.end-screen').removeClass('hidden');
+	
+
+	//if you get a hiscore, take them to the screen for entering in your initials
+	if(score > lowestScore){
+		$('.main').addClass('hidden');
+		$('.enter-hiscore').removeClass('hidden');
+	} else {
+		$('.main').addClass('hidden');
+		$('.end-screen').removeClass('hidden');
+	}
+	//if not, just go right to the end screen
+	
 }
+
+//Pushes variables such as user initials and user score to Firebase
+var saveToFB = function(){
+    database.ref().push({
+    	userInitials: userInitials,
+        score: score,
+        inverseScore: inverseScore,
+        dateAdded: firebase.database.ServerValue.TIMESTAMP
+    })
+};
+
+var sendHiscore = function(){
+	event.preventDefault();
+	userInitials = $('#hiscoreName').val();
+	console.log(userInitials);
+	//Go to the end screen
+	$('.enter-hiscore').addClass('hidden');
+	$('.end-screen').removeClass('hidden');
+	inverseScore = 1 / score;
+	//Send all the information gathered to Firebase
+	saveToFB();
+	//Generates the hiscore table (hopefully before the user gets there)
+	generateTable();
+};
+
+var generateTable = function(){
+	//Generate the hiscore table using the first 10 values in Firebase
+database.ref().orderByChild("inverseScore").limitToLast(10).on("child_added", function(snapshot){
+    var sv = snapshot.val();
+    //---This is where the magic happens
+    var rowHold = $("<tr>");
+
+    var rowInitials = $("<td>");
+    rowInitials.text(sv.userInitials);
+    rowHold.append(rowInitials);
+
+    var rowScore = $("<td>");
+    rowScore.text(sv.score);
+    rowHold.append(rowScore);
+
+    var rowDate = $("<td>");
+    rowDate.text(sv.dateAdded);
+    rowHold.append(rowDate)
+
+    $("tbody").append(rowHold);
+
+    //-------End of the magic
+});
+};
